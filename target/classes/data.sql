e260cd56c5da5008ba6b141fbee3dd8b6e24b7ae
Mask <2421697126@qq.com>
2018-11-20 23:24:04 +0800

    2018-11-20 第八章

---
+++ target/classes/data.sql
@@ -0,0 +1,5 @@
+insert into person(id,name,age,address) values (hibernate_sequence.nextval,'mask',22,'武汉');
+insert into person(id,name,age,address) values (hibernate_sequence.nextval,'aa',22,'北京');
+insert into person(id,name,age,address) values (hibernate_sequence.nextval,'bb',22,'上海');
+insert into person(id,name,age,address) values (hibernate_sequence.nextval,'cc',22,'广州');
+insert into person(id,name,age,address) values (hibernate_sequence.nextval,'dd',22,'杭州');
